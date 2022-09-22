55c6d3820c8eece9daba7149ddf254ac5db5f5da
ho1447 <ho1447@users.noreply.github.com>
2022-09-22 17:22:12 -0400

    feat: read csv file

--- labs/02-lab/cs506/read.py
+++ labs/02-lab/cs506/read.py
@@ -1,6 +1,16 @@
+import csv
+
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    with open(csv_file_path) as input_file:
+        read_input = csv.reader(input_file)
+        input_matrix = []
+        for row in read_input:
+            row = [int(item) if item.isnumeric() else item for item in row]
+            input_matrix.append(row)
+
+
+    return input_matrix
